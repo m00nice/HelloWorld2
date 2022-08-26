31e4143e0eacad18a7295017c5b644734c4ee5c2
m00ni <m00nice@live.com>
2022-08-26 08:55:11 +0200

    Initial commit

---
+++ src/main/java/com/example/simplehelloworld2/SimpleHelloWorld2Application.java
@@ -0,0 +1,13 @@
+package com.example.simplehelloworld2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHelloWorld2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleHelloWorld2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplehelloworld2/SimpleHelloWorld2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplehelloworld2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHelloWorld2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
